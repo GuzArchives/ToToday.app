d1e966a7cca8a48458eaf97b37276c530fe091e4
Guz <43732358+Guz013@users.noreply.github.com>
2022-01-26 12:31:16 -0300

    feat(dev, footer): added info about the storage file
    Added when the memory/storage file was last updated to the footer and changed copyright from Project Lored to simply Lored.

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,10 +1,10 @@
 <template>
 	<div class="footer">
 		<p>
-			Version: {{ version }}
+			Version: {{ version }} {{ storage_updated }}
 			<br />
 			<a href="http://github.com/ProjectLored/ToToday.app" target="_blank">
-				(c) 2022 Project Lored &bull; MIT License
+				(c) 2022 Lored &bull; MIT License
 			</a>
 			<br />
 			Icons provided by
@@ -16,12 +16,18 @@
 <script lang="ts">
 import Vue from 'vue';
 import appInfo from '~~/package.json';
+import sm from '~/libs/storageManagement';
+
+const storageUpdated = sm.get(true)?.date ? `- Storage Updated: ${sm.get(true).date.updated.day.readable} ${sm.get(true).date.updated.hour.readable}` : '';
 
 export default Vue.extend({
 	name: 'PageFooter',
 	data() {
-		return {version: appInfo.version};
-	}
+		return {
+			version: appInfo.version,
+			storage_updated: storageUpdated,
+		};
+	},
 });
 </script>
 
