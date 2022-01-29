b2e4b822e8e3540c1798c129fb5135736a194291
Guz <43732358+Guz013@users.noreply.github.com>
2022-01-29 11:39:50 -0300

    debug: removed buttons used for debugging the task component

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,10 +4,9 @@
 			<ToTodayLogo />
 			<p>A single page web app to help yours day-to-day tasks</p>
 			<ThemePicker />
-			<button @click="addItem()">Add storage</button>
-			<button @click="setItem()">Set storage</button>
-			<button @click="resetStorage()">Reset</button>
-			<button @click="invalidate()">Invalidate</button>
+		</div>
+		<div>
+			<TaskList/>
 		</div>
 		<PageFooter />
 	</main>
@@ -15,23 +14,7 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import sm from '~/libs/storageManagement';
-
 export default Vue.extend({
 	name: 'IndexPage',
-	methods: {
-		addItem: () => {
-			sm.add('test1', 3);
-		},
-		setItem: () => {
-			sm.set('test1', sm.get().test1 + 1);
-		},
-		resetStorage: () => {
-			sm.createJSON(true);
-		},
-		invalidate: () => {
-			localStorage.setItem('ToToday-storage', '{"aaa": "aaa"}');
-		},
-	},
 });
 </script>
